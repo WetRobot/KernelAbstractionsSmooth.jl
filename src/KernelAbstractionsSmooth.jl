e6837eb4608b7179e4ac89762604f915216a7970
Joonas Miettinen <joonas.m.miettinen@gmail.com>
2023-11-24 20:08:41 +0200

    feat: enable importance weights

--- src/KernelAbstractionsSmooth.jl
+++ src/KernelAbstractionsSmooth.jl
@@ -19,13 +19,14 @@ module KernelAbstractionsSmooth
         i::Integer,
         J::Integer,
         get_fit_y_j::Function,
-        eval_y_hat::AbstractVector, 
+        eval_y_hat::AbstractVector,
         theta::Real
+        ;
+        ulw_j::Nothing = nothing
     )::Nothing
         w_i = ulw_i_j.(i, 1:J, theta)
         eval_y_hat_i = 0.0
         lse = lsexp.log_sum_exp(w_i)
-        w_i .-= lse
         for j in 1:J
             eval_y_hat_i += get_fit_y_j(j) * exp(w_i[j] - lse)
         end
@@ -33,17 +34,49 @@ module KernelAbstractionsSmooth
         nothing
     end
 
+    function compute_eval_y_hat_i(
+        ulw_i_j::Function,
+        i::Integer,
+        J::Integer,
+        get_fit_y_j::Function,
+        eval_y_hat::AbstractVector,
+        theta::Real
+        ;
+        ulw_j::Function
+    )::Nothing
+        w_i = ulw_i_j.(i, 1:J, theta)
+        w_i .+= ulw_j.(1:J)
+        eval_y_hat_i = 0.0
+        lse = lsexp.log_sum_exp(w_i)
+        for j in 1:J
+            eval_y_hat_i += get_fit_y_j(j) * exp(w_i[j] - lse)
+        end
+        eval_y_hat[i] = eval_y_hat_i
+        nothing
+    end
+        
     ka.@kernel function ka_smoothing_kernel(
         ulw_i_j::Function,
         J::Integer,
         get_fit_y_j::Function,
         eval_y_hat::AbstractVector, 
-        theta::Real
+        theta::Real,
+        ulw_j::Union{Function, Nothing}
     )::Nothing
         i = @index(Global)
-        compute_eval_y_hat_i(ulw_i_j, i, J, get_fit_y_j, eval_y_hat, theta)
+        compute_eval_y_hat_i(
+            ulw_i_j,
+            i,
+            J,
+            get_fit_y_j,
+            eval_y_hat,
+            theta
+            ;
+            ulw_j = ulw_j
+        )
         nothing
     end
+    
 
     function ka_smooth!(
         ulw_i_j::Function,
@@ -52,6 +85,7 @@ module KernelAbstractionsSmooth
         eval_y_hat::AbstractVector, 
         theta::Real
         ;
+        ulw_j::Union{Function, Nothing} = nothing,
         n_jobs::Int = 1
     )::Nothing
         ka_backend = ka.get_backend(eval_y_hat)
@@ -66,10 +100,12 @@ module KernelAbstractionsSmooth
             get_fit_y_j,
             eval_y_hat, 
             theta,
+            ulw_j
+            ;
             ndrange = length(eval_y_hat)
         )
         synchronize(ka_backend)
-        return(nothing)
+        nothing
     end 
 
     function ka_smooth!(
@@ -80,6 +116,7 @@ module KernelAbstractionsSmooth
         eval_y_hat::AbstractVector, 
         theta::Real
         ;
+        ulw_j::Union{Function, Nothing} = nothing,
         n_jobs::Int = 1
     )::Nothing
         function ulw_i_j(i::Integer, j::Integer, theta::Real)::Real
@@ -97,8 +134,9 @@ module KernelAbstractionsSmooth
             length(fit_y),
             get_fit_y_j,
             eval_y_hat, 
-            theta
+            theta,
             ;
+            ulw_j = ulw_j,
             n_jobs = n_jobs
         )
         nothing
